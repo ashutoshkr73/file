e899c527220e17f1d1521d327d9f9f7fa5b2c195
Ashutosh Kumar <ashu7352952921@gmail.com>
2021-05-22 17:42:40 +0530

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 
 const connectDB = require("./config/db");
+
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
+
+// cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENT.split(",")
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
